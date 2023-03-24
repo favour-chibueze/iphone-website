ace00e46b529a171e0bedb59e2611fef408e89e2
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2023-03-24 19:02:22 +0100

    Feat: added input validations and mobile responsivenes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Header section -->
-    <header class="container">
+    <header class="app-container">
       <nav class="navbar">
         <div>
           <img src="@/assets/images/ips-logo.svg" />
@@ -10,8 +10,8 @@
     </header>
 
     <!-- Wait list section  -->
-    <div class="container wait-list-wrapper">
-      <div class="wait-list-form-wrapper">
+    <div class="app-container wait-list-wrapper row">
+      <div class="wait-list-form-wrapper col-md-6 col-sm-12 col-xs-12 col-lg-6">
         <div class="wait-list-badge">New course</div>
         <div>
           <h2 class="wait-list-header">iPhone Photo Academy</h2>
@@ -21,24 +21,37 @@
           </p>
         </div>
         <div class="wait-list-form">
-          <div class="wait-list-input-wrapper">
+          <div
+            class="wait-list-input-wrapper"
+            :class="[emailError ? 'form-input-error' : null]"
+          >
             <div class="wait-list-input-container">
               <input
                 class="wait-list-input"
                 type="text"
-                placeholder="Enter your Email Address"
+                placeholder=" "
+                v-model="email"
               />
+              <label
+                for="wait-list-input"
+                :class="[emailError ? 'form-text-error' : null]"
+                >{{
+                  emailError ? emailError : "Enter your Email Address"
+                }}</label
+              >
             </div>
             <div class="wait-list-form-placeholder--icon">
               <img src="@/assets/images/mail-icon.svg" />
             </div>
           </div>
           <div>
-            <button class="ips-btn">Please Notify Me</button>
+            <button class="ips-btn" @click="submitForm">
+              Please Notify Me
+            </button>
           </div>
         </div>
       </div>
-      <div class="ips-carousel-wrapper">
+      <div class="ips-carousel-wrapper col-md-6 col-md-6 col-sm-12 col-xs-12 col-lg-6">
         <div class="ips-carousel-mockup-wrapper">
           <img src="@/assets/images/iPhone-mokup.png" />
         </div>
@@ -48,12 +61,49 @@
 </template>
 
 <script>
+import { toRefs, reactive, watch } from "vue";
+
 export default {
-  name: "App",
-  components: {},
+  setup() {
+    const state = reactive({
+      email: "",
+      emailError: "",
+      formValid: false,
+    });
+
+    const validateEmail = () => {
+      const isValid = /\S+@\S+\.\S+/.test(state.email);
+      if (!state.email) {
+        state.emailError = "Enter your Email Address";
+        state.formValid = false;
+      } else if (!isValid) {
+        state.emailError = "Please enter a valid email";
+        state.formValid = false;
+      } else {
+        state.emailError = "";
+        state.formValid = true;
+      }
+    };
+
+    const submitForm = () => {
+      validateEmail()
+      if (!state.formValid) {
+        console.log("Form Error");
+      } else {
+        console.log("Form submitted");
+      }
+    };
+
+    watch(() => state.email, validateEmail);
+
+    return {
+      submitForm,
+      ...toRefs(state),
+    };
+  },
 };
 </script>
 
 <style>
-  @import "@/assets/styles/styles.css"
+@import "@/assets/styles/styles.css";
 </style>
