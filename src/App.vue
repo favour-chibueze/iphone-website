14d7e20508abe9a3709a732f1d107f44fbb8aa1f
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2023-03-24 06:07:38 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Header section -->
+    <header class="container">
+      <nav class="navbar">
+        <div>
+          <img src="@/assets/images/ips-logo.svg" />
+        </div>
+      </nav>
+    </header>
+
+    <!-- Wait list section  -->
+    <div class="container wait-list-wrapper">
+      <div class="wait-list-form-wrapper">
+        <div class="wait-list-badge">New course</div>
+        <div>
+          <h2 class="wait-list-header">iPhone Photo Academy</h2>
+          <p class="wait-list-para">
+            Sign up now to get notified <br />
+            when this course is available!
+          </p>
+        </div>
+        <div class="wait-list-form">
+          <div class="wait-list-input-wrapper">
+            <div class="wait-list-input-container">
+              <input
+                class="wait-list-input"
+                type="text"
+                placeholder="Enter your Email Address"
+              />
+            </div>
+            <div class="wait-list-form-placeholder--icon">
+              <img src="@/assets/images/mail-icon.svg" />
+            </div>
+          </div>
+          <div>
+            <button class="ips-btn">Please Notify Me</button>
+          </div>
+        </div>
+      </div>
+      <div class="ips-carousel-wrapper">
+        <div class="ips-carousel-mockup-wrapper">
+          <img src="@/assets/images/iPhone-mokup.png" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "@/assets/styles/styles.css"
 </style>
